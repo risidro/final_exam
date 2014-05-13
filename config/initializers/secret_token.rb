aee045b02540fddf0e75057fac868997ed947d42
risidro <risidro120478@gmail.com>
2014-05-13 18:58:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinalExam::Application.config.secret_key_base = '1e1bbbe81997a58bf6c3c1873ad223f872b0771141d29fb43412d09b2b64435dd8f8de94104a20b8c3c4531c7a4d5c50007b2742eab723de177d31b809dcdb98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FinalExam::Application.config.session_store :cookie_store, key: '_final_exam_session'
